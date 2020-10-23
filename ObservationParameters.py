07f4a761cf57c055f3e8b48663204ad9d47347c6
Michael Lindner <mlindner@uw.edu>
2020-10-23 15:53:58 -0700

    Documentation

--- ObservationParameters.py
+++ ObservationParameters.py
@@ -2,9 +2,15 @@
 
 import pynbody
 
+# Tipsy snapshot to transform into observation frame
 snapshot = pynbody.load('./Data/TheMice.tipsy.005000')
+
+# Length scale factor
 L = 39.5
+
+# Velocity scale factor
 V = 165
+
 # Viewing angle relative to orbital plane
 w = 1.36136
 i = -0.767945
