6a56f454c6b3a09823809abea01c20ca98f6d5a2
Michael Lindner <mlindner@uw.edu>
2021-04-10 22:26:54 -0700

    Brought up to date

--- ObservationParameters.py
+++ ObservationParameters.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-import pynbody
-
-# Tipsy snapshot to transform into observation frame
-#snapshot = pynbody.load('./Runs/MiceScaledMass/MiceScaledMass.003654')
-
 # Viewing angle relative to orbital plane
 thetax = 1.36136
 thetay = -0.767945
